0a53dbec23d7dcaa513ef8815e8b6eaf1fd2c255
izuutech <izutechsofficial@gmail.com>
2022-11-07 13:26:33 +0100

    start woking on index.js

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
 const express = require("express");
+const app = express();
+const tablehead = require("./tablehead.json");
+const data = require("./data.json");
+
+//set view engine
+app.set("view engine", "ejs");
+
+app.listen(5000, () => {
+  console.log("App listening on port 5000!");
+});
+
+console.log(Object.entries(tablehead));
+console.log(data);
+app.get("/", (req, res) => {
+  const keys = Object.entries(tablehead);
+  res.render("index", { data, tablehead: keys });
+});
